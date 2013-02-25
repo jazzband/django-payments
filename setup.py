b799fea736a4889cf2bd62625f21b4bed66ef5e7
Patryk Zawadzki <patrys@room-303.com>
2013-02-25 17:42:25 +0100

    Cosmetics, add Sphinx

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
  
-setup(name='django-payments',
-      author='Mirumee Software',
-      author_email='hello@mirumee.com',
-      description='Universal payment handling for Django',
-      version='2013.2',
-      url='http://github.com/mirumee/django-payments',
-      packages=find_packages(),
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Environment :: Web Environment',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Framework :: Django',
-      ],
-      include_package_data=True,
-      zip_safe=False)
+setup(
+    name='django-payments',
+    author='Mirumee Software',
+    author_email='hello@mirumee.com',
+    description='Universal payment handling for Django',
+    version='2013.2',
+    url='http://github.com/mirumee/django-payments',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    setup_requires=['Sphinx'],
+    include_package_data=True,
+    zip_safe=False)
