b0afef40eaab2c6340595f022ac02b0cb95dca36
Michal Ociepka <michal@ociepka.info>
2013-04-10 15:13:56 +0200

    Add paypal credit card payment

---
+++ payments/paypal-card/__init__.py
@@ -0,0 +1,41 @@
+from .. import BasicProvider, RedirectNeeded
+from ..models import Payment
+from ..paypal import PaypalProvider
+from datetime import timedelta
+from django.contrib.sites.models import Site
+from django.http import HttpResponseForbidden
+from django.shortcuts import redirect
+from django.utils import simplejson, timezone
+import requests
+import urlparse
+from .forms import PaymentForm
+from re import match
+
+
+class PaypalCardProvider(PaypalProvider):
+    '''
+    paypal.com credit card payment provider
+    '''
+    def get_form(self, data=None):
+        return PaymentForm(data, provider=self, payment=self.payment)
+
+    def get_product_data(self, extra_data):
+        data = self.get_transactions_data()
+        year = extra_data['expiration'].year
+        month = extra_data['expiration'].month
+        data['payer'] = {
+            'payment_method': 'credit_card',
+            'funding_instruments': [{
+                'credit_card': {
+                    'number': extra_data['number'],
+                    'type': extra_data['type'],
+                    'expire_month': month,
+                    'expire_year': year,
+                    'cvv2': extra_data['cvv2']
+                }
+             }]
+        }
+        return data
+
+    def process_data(self, request):
+        raise HttpResponseForbidden('FAILED')
