cd793d935631cabb0fbb02110413e72017be08fc
Michal Ociepka <michal@ociepka.info>
2013-04-10 15:12:53 +0200

    Require provider and payment in base form

--- payments/forms.py
+++ payments/forms.py
@@ -1,21 +1,28 @@
 from django import forms
 
+
 class PaymentForm(forms.Form):
     '''
     Payment form, suitable for Django templates.
-    
+
     When displaying the form remeber to use *action* and *method*.
     '''
-    
+
     #: Form action URL for template use
     action = ''
     #: Form method for template use, either "get" or "post"
     method = 'post'
 
-    def __init__(self, data, action, method = 'post'):
-        super(PaymentForm, self).__init__(auto_id = False)
+    def __init__(self, data=None, action=None, method='post', provider=None,
+                 payment=None, hidde_inputs=True):
+        if hidde_inputs:
+            super(PaymentForm, self).__init__(auto_id=False)
+            for key, val in data.items():
+                widget = forms.widgets.HiddenInput()
+                self.fields[key] = forms.CharField(initial=val, widget=widget)
+        else:
+            super(PaymentForm, self).__init__(data=data)
         self.action = action
         self.method = method
-        for key, val in data.items():
-            self.fields[key] = forms.CharField(initial=val, widget=forms.widgets.HiddenInput())
-
+        self.provider = provider
+        self.payment = payment
