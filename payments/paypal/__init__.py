a84802d682fb88e2f35034a21b428a96e559b61f
Michal Ociepka <michal@ociepka.info>
2013-04-26 19:14:18 +0200

    Move paypal-cart into paypal backend

--- payments/paypal/__init__.py
+++ payments/paypal/__init__.py
@@ -5,6 +5,7 @@ from django.shortcuts import redirect
 from django.utils import simplejson, timezone
 import requests
 from functools import wraps
+from .forms import PaymentForm
 
 Payment = get_payment_model()
 
@@ -158,3 +159,33 @@ class PaypalProvider(BasicProvider):
         self.payment.change_status('confirmed')
         self.payment.save()
         return redirect(success_url)
+
+
+class PaypalCardProvider(PaypalProvider):
+    '''
+    paypal.com credit card payment provider
+    '''
+    def get_form(self, data=None):
+        if self.payment.status == 'waiting':
+            self.payment.change_status('input')
+        form = PaymentForm(data, provider=self, payment=self.payment)
+        if form.is_valid():
+            raise RedirectNeeded(self.payment.get_success_url())
+        return form
+
+    def get_product_data(self, extra_data):
+        data = self.get_transactions_data()
+        year = extra_data['expiration'].year
+        month = extra_data['expiration'].month
+        credit_card = {'number': extra_data['number'],
+                       'type': extra_data['type'],
+                       'expire_month': month,
+                       'expire_year': year}
+        if 'cvv2' in extra_data and extra_data['cvv2']:
+            credit_card['cvv2'] = extra_data['cvv2']
+        data['payer'] = {'payment_method': 'credit_card',
+                         'funding_instruments': [{'credit_card': credit_card}]}
+        return data
+
+    def process_data(self, request):
+        return HttpResponseForbidden('FAILED')
