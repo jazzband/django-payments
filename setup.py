2252d492af616dd14c3bf07db7bfda5b60783b41
Patryk Zawadzki <patrys@room-303.com>
2013-04-26 19:49:33 +0200

    0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Mirumee Software',
     author_email='hello@mirumee.com',
     description='Universal payment handling for Django',
-    version='0.2.1',
+    version='0.3',
     url='http://github.com/mirumee/django-payments',
     packages=find_packages(),
     classifiers=[
