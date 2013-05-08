cc5e289a121d441293916e72ce34bef392856aed
Adam Bogdal <adam@bogdal.pl>
2013-05-08 14:34:11 +0200

    Add authorize.net backend

--- payments/forms.py
+++ payments/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+from django.core import validators
+from django.utils.translation import ugettext_lazy as _
+from .fields import CreditCardNumberField, CreditCardExpiryField
+from re import match
 
 
 class PaymentForm(forms.Form):
@@ -26,3 +30,49 @@ class PaymentForm(forms.Form):
         self.method = method
         self.provider = provider
         self.payment = payment
+
+
+class CreditCardPaymentForm(PaymentForm):
+
+    CVV_VALIDATOR = validators.RegexValidator('^[0-9]{1,4}$',
+                                              _('Enter a valid security number.'))
+
+    number = CreditCardNumberField(label=_('Card Number'), max_length=32,
+                                   required=True)
+    expiration = CreditCardExpiryField()
+    cvv2 = forms.CharField(validators=[CVV_VALIDATOR], required=False,
+                           label=_('CVV2 Security Number'), max_length=4)
+
+    default_error_messages = {
+        'invalid_type': _(u'We accept only %(valid_types)s')}
+
+    def __init__(self, *args, **kwargs):
+        super(CreditCardPaymentForm, self).__init__(hidden_inputs=False, *args, **kwargs)
+
+    def get_credit_card_type(self, number):
+        if match('^4[0-9]{12}(?:[0-9]{3})?$', number):
+            return 'visa'
+        if match('^5[1-5][0-9]{14}$', number):
+            return 'mastercard'
+        if match('^6(?:011|5[0-9]{2})[0-9]{12}$', number):
+            return 'discover'
+        if match('^3[47][0-9]{13}$', number):
+            return 'amex'
+        if match('^(?:(?:2131|1800|35\d{3})\d{11})$', number):
+            return 'jcb'
+        if match('^(?:3(?:0[0-5]|[68][0-9])[0-9]{11})$', number):
+            return 'diners club'
+
+    def clean(self):
+        cleaned_data = super(PaymentForm, self).clean()
+
+        if 'number' in cleaned_data.keys():
+            card_type = self.get_credit_card_type(cleaned_data['number'])
+            if card_type not in self.VALID_TYPES:
+                message = (self.default_error_messages['invalid_type'] %
+                           {'valid_types': ', '.join(self.VALID_TYPES)})
+                self._errors['number'] = self.error_class([message])
+            else:
+                self.card_type = card_type
+
+        return cleaned_data
--- payments/paypal/forms.py
+++ payments/paypal/forms.py
@@ -1,51 +1,22 @@
-from ..forms import PaymentForm
-from .fields import CreditCardExpiryField, CreditCardNumberField
 from django import forms
-from django.core import validators
 from django.utils.translation import ugettext_lazy as _
-from re import match
 
-CVV_VALIDATOR = validators.RegexValidator('^[0-9]{1,4}$',
-                                          _('Enter a valid security number.'))
+from ..forms import CreditCardPaymentForm
 
 
-class PaymentForm(PaymentForm):
+class PaymentForm(CreditCardPaymentForm):
 
     VALID_TYPES = ['visa', 'mastercard', 'discover', 'amex']
+
     name = forms.CharField(label=_('Name on Credit Card'), max_length=128)
-    number = CreditCardNumberField(label=_('Card Number'), max_length=32,
-                                   required=True)
-    expiration = CreditCardExpiryField()
-    cvv2 = forms.CharField(validators=[CVV_VALIDATOR], required=False,
-                           label=_('CVV2 Security Number'), max_length=4)
-    default_error_messages = {
-        'invalid_type': _(u'We accept only %(valid_types)s')}
-
-    def __init__(self, *args, **kwargs):
-        super(PaymentForm, self).__init__(hidden_inputs=False, *args, **kwargs)
-
-    def get_credit_card_type(self, number):
-        if match('^4[0-9]{12}(?:[0-9]{3})?$', number):
-            return 'visa'
-        if match('^5[1-5][0-9]{14}$', number):
-            return 'mastercard'
-        if match('^6(?:011|5[0-9]{2})[0-9]{12}$', number):
-            return 'discover'
-        if match('^3[47][0-9]{13}$', number):
-            return 'amex'
 
     def clean(self):
         cleaned_data = super(PaymentForm, self).clean()
-        if 'number' in cleaned_data.keys():
-            type = self.get_credit_card_type(cleaned_data['number'])
-            if type not in self.VALID_TYPES:
-                message = (self.default_error_messages['invalid_type'] %
-                           {'valid_types': ', '.join(self.VALID_TYPES)})
-                self._errors['number'] = self.error_class([message])
-            else:
-                cleaned_data['type'] = type
+
         if not self.errors:
             if not self.payment.transaction_id:
+                request_data = {'type': self.card_type}
+                request_data.update(cleaned_data)
                 response = self.provider.get_payment_response(cleaned_data)
                 data = response.json()
                 if response.ok:
