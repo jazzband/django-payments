e5fd9efde12578f7ac3034e09fe2bd7883701549
Mateusz Dereniowski <derenio@gmail.com>
2015-08-11 15:27:37 +0200

    Return error on a repeated Stripe's payment

--- payments/stripe/forms.py
+++ payments/stripe/forms.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django import forms
+from django.utils.translation import ugettext as _
 import stripe
 
 from ..forms import PaymentForm as BasePaymentForm
@@ -23,21 +24,25 @@ class PaymentForm(BasePaymentForm):
     def clean(self):
         data = self.cleaned_data
 
-        if not self.errors and not self.payment.transaction_id:
-            stripe.api_key = self.provider.secret_key
-            try:
-                self.charge = stripe.Charge.create(
-                    capture=False,
-                    amount=int(self.payment.total * 100),
-                    currency=self.payment.currency,
-                    card=data['stripeToken'],
-                    description='%s %s' % (
-                        self.payment.billing_last_name,
-                        self.payment.billing_first_name))
-            except stripe.CardError as e:
-                # The card has been declined
-                self._errors['__all__'] = self.error_class([e])
-                self.payment.change_status('error', e.message)
+        if not self.errors:
+            if not self.payment.transaction_id:
+                stripe.api_key = self.provider.secret_key
+                try:
+                    self.charge = stripe.Charge.create(
+                        capture=False,
+                        amount=int(self.payment.total * 100),
+                        currency=self.payment.currency,
+                        card=data['stripeToken'],
+                        description='%s %s' % (
+                            self.payment.billing_last_name,
+                            self.payment.billing_first_name))
+                except stripe.CardError as e:
+                    # The card has been declined
+                    self._errors['__all__'] = self.error_class([e])
+                    self.payment.change_status('error', e.message)
+            else:
+                msg = _('This payment has already been processed.')
+                self._errors['__all__'] = self.error_class([msg])
 
         return data
 
