451ad9bc6b08bcc1609f8adcb18f685367ab7230
alex <devkral@web.de>
2017-10-07 10:05:38 +0200

    fix externalpayments, add tests

--- payments/externalpayments/__init__.py
+++ payments/externalpayments/__init__.py
@@ -75,13 +75,13 @@ class BankTransferProvider(BasicProvider):
 
     def __init__(self, iban, bic, confirm=False,
                  prefix="", **kwargs):
-        if len(iban) <= 10 or len(bic) != 11:
+        if len(iban) <= 10 or len(bic) <= 4:
             raise ImproperlyConfigured("Wrong IBAN or BIC")
         self.iban = iban.upper()
         self.bic = bic.upper()
         self.confirm = confirm
         self.prefix = prefix
-        super(AdvancePaymentProvider, self).__init__(**kwargs)
+        super(BankTransferProvider, self).__init__(**kwargs)
         if not self._capture:
             raise ImproperlyConfigured(
                 'Advance Payment does not support pre-authorization.')
--- payments/externalpayments/forms.py
+++ payments/externalpayments/forms.py
@@ -1,4 +1,6 @@
 from django.utils.translation import ugettext_lazy as _
+from django import forms
+
 from ..forms import PaymentForm
 
 class OrderIdForm(PaymentForm):
