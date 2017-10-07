14a3dd79aa82d8afee96c70f69357ee244f092fa
alex <devkral@web.de>
2017-10-07 11:10:42 +0200

    update decimalsfields to have max 20 digits, with 8 decimal places to map btc and cheaper currencies (12 places)

--- payments/models.py
+++ payments/models.py
@@ -54,10 +54,10 @@ class BasePayment(models.Model):
     #: Currency code (may be provider-specific)
     currency = models.CharField(max_length=10)
     #: Total amount (gross)
-    total = models.DecimalField(max_digits=9, decimal_places=2, default='0.0')
-    delivery = models.DecimalField(
-        max_digits=9, decimal_places=2, default='0.0')
-    tax = models.DecimalField(max_digits=9, decimal_places=2, default='0.0')
+    total = models.DecimalField(max_digits=20, decimal_places=8, default='0.0')
+    delivery = models.DecimalField(max_digits=20,
+        decimal_places=8, default='0.0')
+    tax = models.DecimalField(max_digits=20, decimal_places=8, default='0.0')
     description = models.TextField(blank=True, default='')
     billing_first_name = models.CharField(max_length=256, blank=True)
     billing_last_name = models.CharField(max_length=256, blank=True)
@@ -73,7 +73,7 @@ class BasePayment(models.Model):
     message = models.TextField(blank=True, default='')
     token = models.CharField(max_length=36, blank=True, default='')
     captured_amount = models.DecimalField(
-        max_digits=9, decimal_places=2, default='0.0')
+        max_digits=20, decimal_places=8, default='0.0')
 
     class Meta:
         abstract = True
