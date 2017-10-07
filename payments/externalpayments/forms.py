e01551eadc7cd6c14075252e9a88a8c13fd8da56
alex <devkral@web.de>
2017-10-07 09:14:06 +0200

    placeholder provider: DirectPayment and BankTransfer

---
+++ payments/externalpayments/forms.py
@@ -0,0 +1,15 @@
+from django.utils.translation import ugettext_lazy as _
+from ..forms import PaymentForm
+
+class OrderIdForm(PaymentForm):
+    # only shown, return is ignored
+    order = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}),\
+                            label=_("Please supply as reference"))
+
+
+class IBANBankingForm(PaymentForm):
+    # only shown, return is ignored
+    iban = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}), label="IBAN")
+    bic = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}), label="BIC")
+    order = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}),\
+                                                   label=_("Please supply as reference"))
