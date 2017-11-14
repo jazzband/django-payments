afcf18e5075ede3519dc27415a3c1d239016cc5b
Patryk Zawadzki <patrys@room-303.com>
2017-11-14 14:46:26 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ REQUIREMENTS = [
 class PyTest(TestCommand):
     user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
     test_args = []
-    
+
     def initialize_options(self):
         TestCommand.initialize_options(self)
         self.pytest_args = []
@@ -56,7 +56,7 @@ setup(
     author='Mirumee Software',
     author_email='hello@mirumee.com',
     description='Universal payment handling for Django',
-    version='0.12.0',
+    version='0.12.1',
     url='http://github.com/mirumee/django-payments',
     packages=PACKAGES,
     include_package_data=True,
