49da13fed8c855f8dc72c0203f6ce7a0eb38bef0
pouya-barzegar <poooooyabarzegar@gmail.com>
2018-06-17 10:11:38 +0430

    at last, a working sample, better be used with some custom settings in saleor

--- payments/zarinpal/__init__.py
+++ payments/zarinpal/__init__.py
@@ -6,6 +6,10 @@ from django.shortcuts import redirect
 from django.core.exceptions import ImproperlyConfigured
 from django.http import HttpResponse
 
+
+from collections import OrderedDict
+
+
 from zeep.client import Client
 
 
@@ -16,59 +20,65 @@ class ZarinpalProvider(BasicProvider):
         self._merchant_code = merchant_code
         self._web_service = web_service
         self._capture = True
-        self.USE_SSL = False
+        self.USE_SSL = True
 
         if not self._capture:
             raise ImproperlyConfigured(
-                'Sagepay does not support pre-authorization.')
+                'Zarinpal service does not support pre-authorization.')
+
 
     def get_form(self, payment, data=None):
         client = Client(self._web_service)
         # converting rial to tooman
-        amount = payment.total / 10
-        description = "صفحه ی خرید"
-        CallbackURL = "https://arput.com"
+        amount = int(payment.total // 10)
+        
+        description = payment.description
+        CallbackURL = payment.get_return_url()
         result = client.service.PaymentRequest(MerchantID=self._merchant_code,
                                                Amount=amount,
                                                #    email,
                                                #    mobile,
                                                Description=description,
                                                CallbackURL=CallbackURL)
+
+
+        redirect_url = 'https://www.zarinpal.com/pg/StartPay/' + str(result.Authority)
+
+
         if result.Status == 100:
-            payment.changestatus(PaymentStatus.INPUT)
-
-        form = PaymentForm(data=data, payment=payment, provider=self)
-
-        if form.is_valid():
-            raise RedirectNeeded(payment.get_success_url())
-        return form
+            payment.change_status(PaymentStatus.INPUT)
+            raise RedirectNeeded(redirect_url)
 
 
-    def get_action(self, payment, request):
-        # print(":::::::::::::verify payment:::::::::")
-        # amount = payment.total
-        # pay = payment.changestatus(PaymentStatus.CONFIRMED)
-        # client = Client(self._web_service)
-        # if request.GET.get('Status') == 'OK':
-        #     result = client.service.PaymentVerification(self._merchant_code,
-        #                                                 request.GET['Authority'],
-        #                                                 amount)
-        #     if result.Status == 100:
-        #         pay = payment.changestatus(PaymentStatus.CONFIRMED)
-        #         # return HttpResponse('Transaction success. RefID: ' + str(result.RefID))
-        #     elif result.Status == 101:
-        #         payment = payment.changestatus(PaymentStatus.CONFIRMED)
-        #         # return HttpResponse('Transaction submitted : ' + str(result.Status))
-        #     else:
-        #         payment = payment.changestatus(PaymentStatus.REJECTED)
-        #         # return HttpResponse('Transaction failed. Status: ' + str(result.Status))
-        # else:
-        #     # return HttpResponse('Transaction failed or canceled by user')
-        #     payment_track_id = result.RefID
-        return
+    def process_data(self, payment, request, **kwargs):
+        # Verifying the payment
+        amount = int(payment.total // 10)
+        pay = payment.change_status(PaymentStatus.CONFIRMED)
+        client = Client(self._web_service)
+        if request.GET.get('Status') == 'OK':
+            result = client.service.PaymentVerification(self._merchant_code,
+                                                        request.GET['Authority'],
+                                                        amount)
+            if result.Status == 100:
+                payment.change_status(PaymentStatus.CONFIRMED)
+                # return HttpResponse('Transaction success. RefID: ' + str(result.RefID))
+            elif result.Status == 101:
+                payment.change_status(PaymentStatus.WAITING)
+                # return HttpResponse('Transaction submitted : ' + str(result.Status))
+                
+            else:
+                payment.change_status(PaymentStatus.REJECTED)
+                # return HttpResponse('Transaction failed. Status: ' + str(result.Status))
+        else:
+            # return HttpResponse('Transaction failed or canceled by user')
+            payment_track_id = result.RefID
+
+        payment.save()
+        return 
+
 
     def capture(self, payment, amount=None):
-        payment.changestatus(PaymentStatus.CONFIRMED)
+        payment.change_status(PaymentStatus.CONFIRMED)
         return amount
 
     def release(self, payment):
