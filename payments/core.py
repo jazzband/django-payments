80e3a5ee0ab9ff0d81eb368d2564b5b630f2049f
Petr Dlouhý <petr.dlouhy@email.cz>
2020-02-14 19:42:09 +0100

    add common subscription methods

--- payments/core.py
+++ payments/core.py
@@ -143,6 +143,27 @@ class BasicProvider:
             return url + "?" + qs
         return url
 
+    def autocomplete_with_subscription(self, payment):
+        """
+        Complete the payment with subscription
+
+        If the provider uses workflow such that the payments are initiated from
+        implementer's side.
+        The users of django-payments will create a payment and call
+        Payment.autocomplete_with_subscription() right before the subscription end.
+
+        Throws RedirectNeeded if there is problem with the payment
+        that needs to be solved by user.
+        """
+        raise NotImplementedError
+
+    def cancel_subscription(self, subscription):
+        """
+        Cancel subscription
+        Used by providers, that use provider initiated cancellation workflow
+        """
+        raise NotImplementedError
+
     def capture(self, payment, amount=None):
         raise NotImplementedError
 
