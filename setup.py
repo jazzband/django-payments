99d74b80c4a56536660c148e2c54fd23f278fd00
Hugo Osvaldo Barrera <hugo@barrera.io>
2020-07-28 22:07:01 +0200

    Support Python 3.8

--- setup.py
+++ setup.py
@@ -75,6 +75,8 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Framework :: Django',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Topic :: Software Development :: Libraries :: Python Modules'],
