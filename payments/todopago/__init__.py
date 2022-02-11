91345836d425d4e954ffab7ddfaaedc9fcdc4914
Juan Pablo Senn <juanpsenn@gmail.com>
2022-02-10 22:21:08 -0300

    TodoPagoProvider payment flow implemented

--- payments/todopago/__init__.py
+++ payments/todopago/__init__.py
@@ -2,13 +2,19 @@ from decimal import Decimal
 from uuid import uuid4
 
 from django.http import HttpRequest
+from django.shortcuts import redirect
 from python_todopago import TodoPagoConnector
 from python_todopago.helpers import Item
 
 from payments import PaymentError
+from payments import PaymentStatus
 from payments import RedirectNeeded
 from payments.core import BasicProvider
 from payments.models import BasePayment
+
+STATUS_MAP = {
+    "APROBADA": PaymentStatus.CONFIRMED,
+}
 
 
 class TodoPagoProvider(BasicProvider):
@@ -75,15 +81,62 @@ class TodoPagoProvider(BasicProvider):
 
         return authorization
 
+    def fetch_operation_status(self, payment: BasePayment):
+        request_key = payment.attrs.request_key
+        answer_key = payment.attrs.answer_key
+
+        status = self.client.get_operation_status(request_key, answer_key)
+
+        if status.status_code != -1:
+            message = "TodoPago didn't approve the payment."
+            payment.change_status(PaymentStatus.ERROR, message)
+            raise (PaymentError(message))
+
+        upstream_status = status.status_message
+        payment.change_status(STATUS_MAP[upstream_status])
+
     def get_action(self, payment: BasePayment):
         # This is the form-action. But we don't use a form.
         raise NotImplementedError()
 
+    def process_callback(self, payment: BasePayment, request: HttpRequest):
+        """Process TodoPago's callback.
+
+        After a payment is made, TodoPago sends a GET request with the
+        answer key. We use this to get the status of the payment.
+
+        If there's no answer key then we assume that the payment failed.
+        """
+        answer_key = request.GET.get("Answer", None)
+        if not answer_key:
+            payment.change_status(PaymentStatus.ERROR)
+            return redirect(payment.get_failure_url())
+
+        payment.attrs.answer_key = answer_key
+        payment.save()
+
+        self.fetch_operation_status(payment)
+
+        return redirect(payment.get_success_url())
+
     def process_data(self, payment: BasePayment, request: HttpRequest):
-        raise NotImplementedError()
+        """Handle a request received after a payment.
+
+        If it's a GET request, then it's the user being redirected after
+        completing a payment (it may have failed or have been successfull).
+
+        TodoPago doesn't send POST requests, at least for now.
+        If the payment is refunded from the TodoPago's platform there's no way
+        we notice that.
+        """
+        if request.method == "GET":
+            return self.process_callback(payment, request)
 
     def get_form(self, payment: BasePayment, data=None):
-        url = self.autorize_operation(payment)
+        if not hasattr(payment.attrs, "form_url"):
+            _ = self.autorize_operation(payment)
+
+        url = payment.attrs.form_url
 
         raise RedirectNeeded(url)
 
