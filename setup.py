298b5839ba155f8778124bae6284b342fcd775bf
Tom Hendrikx <tom@whyscream.net>
2023-01-03 22:03:51 +0100

    Widen the pin for django-phonenumber-field to >=5.0.0
    The 5.0.0 version is just as good for django-payments,
    and more supported versions means better integration with other packages

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     install_requires=[
         "Django>=2.2",
         "requests>=1.2.0",
-        "django-phonenumber-field[phonenumberslite]>=6.0.0",
+        "django-phonenumber-field[phonenumberslite]>=5.0.0",
     ],
     extras_require={
         "braintree": ["braintree>=3.14.0"],
