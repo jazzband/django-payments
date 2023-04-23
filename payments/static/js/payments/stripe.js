a991ca06fadfdac6e7b93d6f99dce448e049a2eb
Mario Hernandez <mariofix@proton.me>
2023-04-23 00:36:54 -0400

    Stripe API Update

--- payments/static/js/payments/stripe.js
+++ payments/static/js/payments/stripe.js
@@ -1,32 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var stripeInput = document.getElementById('id_stripe_token');
-    var form = stripeInput.form;
-    var publishableKey = stripeInput.attributes['data-publishable-key'].value;
-    Stripe.setPublishableKey(publishableKey);
+    console.log("hello there DOMContentLoaded")
+    const stripeInput = document.getElementById('id_stripe_token');
+    const stripe_session_id = document.getElementById('session_id');
+    const form = stripeInput.form;
+    const publishableKey = stripeInput.attributes['data-publishable-key'].value;
+    const stripe = Stripe(publishableKey)
+
     form.addEventListener('submit', function (e) {
+        console.log("hello there submit")
         var button = this.querySelector('[type=submit]');
         button.disabled = true;
-        Stripe.card.createToken({
-            name: this.elements.id_name.value,
-            number: this.elements.id_number.value,
-            cvc: this.elements.id_cvv2.value,
-            exp_month: this.elements.id_expiration_0.value,
-            exp_year: this.elements.id_expiration_1.value,
-            address_line1: stripeInput.attributes['data-address-line1'].value,
-            address_line2: stripeInput.attributes['data-address-line2'].value,
-            address_city: stripeInput.attributes['data-address-city'].value,
-            address_state: stripeInput.attributes['data-address-state'].value,
-            address_zip: stripeInput.attributes['data-address-zip'].value,
-            address_country: stripeInput.attributes['data-address-country'].value
-        }, function (status, response) {
-            if (400 <= status && status <= 500) {
-                alert(response.error.message);
-                button.disabled = false;
-            } else {
-                stripeInput.value = response.id;
-                form.submit();
-            }
-        });
+        stripe.redirectToCheckout({ sessionId: stripe_session_id })
         e.preventDefault();
     }, false);
 }, false);
+console.log("hello there stripe.js")
