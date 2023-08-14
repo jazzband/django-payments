7e9f2a59267ffd6ac10bdb2a2ceb02ddeca2205b
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2023-08-14 22:00:25 +0200

    Implicitly concatenate string literals on one line

--- payments/core.py
+++ payments/core.py
@@ -21,7 +21,7 @@ PAYMENT_HOST = getattr(settings, "PAYMENT_HOST", None)
 if not PAYMENT_HOST:
     if "django.contrib.sites" not in settings.INSTALLED_APPS:
         raise ImproperlyConfigured(
-            "The PAYMENT_HOST setting without " "the sites app must not be empty."
+            "The PAYMENT_HOST setting without the sites app must not be empty."
         )
     from django.contrib.sites.models import Site
 
