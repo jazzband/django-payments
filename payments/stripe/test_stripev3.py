6e6c310377c6d278ecf7db81ad59c883e5472231
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2025-09-02 11:34:32 +0200

    Rewrite Stripe tests in pytest-style

--- payments/stripe/test_stripev3.py
+++ payments/stripe/test_stripev3.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
-from unittest import TestCase
-from unittest import skip
 from unittest.mock import Mock
 from unittest.mock import patch
+
+import pytest
 
 from payments import PaymentError
 from payments import PaymentStatus
@@ -51,115 +51,124 @@ class Payment(Mock):
         return "http://success.com"
 
 
-class TestStripeProviderV3(TestCase):
-    def test_provider_create_session_success(self):
-        payment = Payment()
-        provider = StripeProviderV3(api_key=API_KEY)
-        return_value = {
-            "id": "cs_test_...",
-            "url": "https://checkout.stripe.com/c/pay/cs_test_...",
-            "status": "open",
-            "payment_status": "unpaid",
-            "payment_intent": "pi_...",
-        }
-        with (
-            patch("stripe.checkout.Session.create", return_value=return_value),
-            self.assertRaises(RedirectNeeded),
-        ):
+def test_provider_create_session_success():
+    payment = Payment()
+    provider = StripeProviderV3(api_key=API_KEY)
+    return_value = {
+        "id": "cs_test_...",
+        "url": "https://checkout.stripe.com/c/pay/cs_test_...",
+        "status": "open",
+        "payment_status": "unpaid",
+        "payment_intent": "pi_...",
+    }
+    with (
+        patch("stripe.checkout.Session.create", return_value=return_value),
+        pytest.raises(RedirectNeeded),
+    ):
+        provider.get_form(payment)
+
+    assert "url" in payment.attrs.session
+    assert "id" in payment.attrs.session
+    assert payment.status == PaymentStatus.WAITING
+
+
+def test_provider_create_session_failure():
+    payment = Payment()
+    provider = StripeProviderV3(api_key=API_KEY)
+
+    with patch("stripe.checkout.Session.create") as f_session:
+        f_session.side_effect = PaymentError("Error")
+        with pytest.raises(PaymentError):
             provider.get_form(payment)
-            self.assertTrue("url" in payment.attrs.session)
-            self.assertTrue("id" in payment.attrs.session)
-        self.assertEqual(payment.status, PaymentStatus.WAITING)
 
-    def test_provider_create_session_failure(self):
-        payment = Payment()
-        provider = StripeProviderV3(api_key=API_KEY)
-        return_value = {
-            "status": "open",
-            "payment_status": "unpaid",
-            "payment_intent": "pi_...",
-        }
-        with patch(
-            "stripe.checkout.Session.create", return_value=return_value
-        ) as f_session:
-            f_session.side_effect = PaymentError("Error")
-            with self.assertRaises(PaymentError):
-                provider.get_form(payment)
+    assert payment.status == PaymentStatus.ERROR
 
-            self.assertEqual(payment.status, PaymentStatus.ERROR)
 
-    def test_provider_create_session_failure_no_url(self):
-        payment = Payment()
-        provider = StripeProviderV3(api_key=API_KEY)
-        return_value = {
-            "status": "open",
-            "payment_status": "unpaid",
-            "payment_intent": "pi_...",
-        }
-        with patch("stripe.checkout.Session.create", return_value=return_value):
-            with self.assertRaises(PaymentError):
-                provider.get_form(payment)
+def test_provider_create_session_failure_no_url():
+    payment = Payment()
+    provider = StripeProviderV3(api_key=API_KEY)
+    return_value = {
+        "status": "open",
+        "payment_status": "unpaid",
+        "payment_intent": "pi_...",
+    }
+    with (
+        patch("stripe.checkout.Session.create", return_value=return_value),
+        pytest.raises(PaymentError),
+    ):
+        provider.get_form(payment)
 
-            self.assertFalse("url" in payment.attrs.session)
-            self.assertFalse("id" in payment.attrs.session)
+    assert "url" not in payment.attrs.session
+    assert "id" not in payment.attrs.session
 
-    def test_provider_create_session_failure_with_transaction_id(self):
-        payment = Payment()
-        payment.transaction_id = "transaction-id"
-        provider = StripeProviderV3(api_key=API_KEY)
-        with patch("stripe.checkout.Session.create"), self.assertRaises(PaymentError):
-            provider.create_session(payment)
 
-    @skip("https://github.com/jazzband/django-payments/issues/444")
-    def test_provider_create_session_success_with_billing_name(self):
-        payment = Payment()
-        payment.billing_name = "Billy Ngname"
-        provider = StripeProviderV3(api_key=API_KEY)
+def test_provider_create_session_failure_with_transaction_id():
+    payment = Payment()
+    payment.transaction_id = "transaction-id"
+    provider = StripeProviderV3(api_key=API_KEY)
+    with patch("stripe.checkout.Session.create"), pytest.raises(PaymentError):
         provider.create_session(payment)
 
-    def test_provider_status(self):
-        payment = Payment()
-        provider = StripeProviderV3(api_key=API_KEY)
 
-        class return_value:
-            payment_status = "paid"
+@pytest.mark.skip(reason="https://github.com/jazzband/django-payments/issues/444")
+def test_provider_create_session_success_with_billing_name():
+    payment = Payment()
+    payment.billing_name = "Billy Ngname"
+    provider = StripeProviderV3(api_key=API_KEY)
+    provider.create_session(payment)
 
-        with patch("stripe.checkout.Session.retrieve", return_value=return_value):
-            provider.status(payment)
 
-    def test_provider_refund_failure_bad_status(self):
-        payment = Payment()
-        provider = StripeProviderV3(api_key=API_KEY)
-        with self.assertRaises(PaymentError):
+@pytest.mark.skip(reason="Breaks global state and asserts nothing")
+def test_provider_status():
+    payment = Payment()
+    provider = StripeProviderV3(api_key=API_KEY)
+
+    class return_value:
+        payment_status = "paid"
+
+    with patch("stripe.checkout.Session.retrieve", return_value=return_value):
+        provider.status(payment)
+
+
+def test_provider_refund_failure_bad_status():
+    payment = Payment()
+    provider = StripeProviderV3(api_key=API_KEY)
+    with pytest.raises(PaymentError):
+        provider.refund(payment)
+
+
+def test_provider_refund_failure_no_payment_intent():
+    payment = Payment()
+    payment.status = PaymentStatus.CONFIRMED
+    assert isinstance(payment.attrs.session, dict)
+    del payment.attrs.session["payment_intent"]
+    provider = StripeProviderV3(api_key=API_KEY)
+    with pytest.raises(PaymentError):
+        provider.refund(payment)
+
+
+def test_provider_refund_failure_stripe_error():
+    payment = Payment()
+    payment.status = PaymentStatus.CONFIRMED
+    provider = StripeProviderV3(api_key=API_KEY)
+    with patch("stripe.Refund.create") as f_refund:
+        f_refund.side_effect = PaymentError("Stripe error")
+        with pytest.raises(PaymentError):
             provider.refund(payment)
 
-    def test_provider_refund_failure_no_payment_intent(self):
-        payment = Payment()
-        payment.status = PaymentStatus.CONFIRMED
-        del payment.attrs.session["payment_intent"]
-        provider = StripeProviderV3(api_key=API_KEY)
-        with self.assertRaises(PaymentError):
-            provider.refund(payment)
 
-    def test_provider_refund_failure_stripe_error(self):
-        payment = Payment()
-        payment.status = PaymentStatus.CONFIRMED
+def test_provider_refund_success():
+    payment = Payment()
+    payment.status = PaymentStatus.CONFIRMED
+    payment.attrs.session["payment_intent"] = "pi_..."
+    provider = StripeProviderV3(api_key=API_KEY)
+    return_value = {
+        "id": "re_...",
+        "payment_status": "succeeded",
+        "amount": 100,
+    }
 
-        provider = StripeProviderV3(api_key=API_KEY)
-        with patch("stripe.Refund.create"), self.assertRaises(PaymentError):
-            provider.refund(payment)
+    with patch("stripe.Refund.create", return_value=return_value):
+        provider.refund(payment)
 
-    def test_provider_refund_success(self):
-        payment = Payment()
-        payment.status = PaymentStatus.CONFIRMED
-        payment.attrs.session["payment_intent"] = "pi_..."
-        provider = StripeProviderV3(api_key=API_KEY)
-        return_value = {
-            "id": "re_...",
-            "payment_status": "succeeded",
-            "amount": 100,
-        }
-
-        with patch("stripe.Refund.create", return_value=return_value):
-            provider.refund(payment)
-            self.assertEqual(payment.status, PaymentStatus.REFUNDED)
+    assert payment.status == PaymentStatus.REFUNDED
