4a42c42de472099e6d086f4013d46d25027b7d1b
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2025-09-02 19:03:07 +0200

    Rewrite Sofort tests in pytest-style

--- payments/sofort/test_sofort.py
+++ payments/sofort/test_sofort.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
 import json
-from unittest import TestCase
 from unittest.mock import MagicMock
 from unittest.mock import Mock
 from unittest.mock import patch
+
+import pytest
 
 from payments import PaymentStatus
 from payments import RedirectNeeded
@@ -40,80 +41,88 @@ class Payment(Mock):
         self.status = status
 
 
-class TestSofortProvider(TestCase):
-    def setUp(self):
-        self.payment = Payment()
-        self.provider = SofortProvider(id=CLIENT_ID, project_id=PROJECT_ID, key=SECRET)
+@pytest.fixture
+def payment():
+    return Payment()
 
-    @patch("xmltodict.parse")
-    @patch("requests.post")
-    def test_provider_raises_redirect_needed_on_success(
-        self, mocked_post, mocked_parser
-    ):
-        response = MagicMock()
-        response.status_code = 200
-        mocked_post.return_value = response
-        mocked_parser.return_value = {
-            "new_transaction": {"payment_url": "http://payment.com"}
+
+@pytest.fixture
+def provider():
+    return SofortProvider(id=CLIENT_ID, project_id=PROJECT_ID, key=SECRET)
+
+
+@patch("xmltodict.parse")
+@patch("requests.post")
+def test_provider_raises_redirect_needed_on_success(
+    mocked_post, mocked_parser, payment, provider
+):
+    response = MagicMock()
+    response.status_code = 200
+    mocked_post.return_value = response
+    mocked_parser.return_value = {
+        "new_transaction": {"payment_url": "http://payment.com"}
+    }
+    with pytest.raises(RedirectNeeded):
+        provider.get_form(payment)
+
+
+@patch("xmltodict.parse")
+@patch("requests.post")
+@patch("payments.sofort.redirect")
+def test_provider_redirects_on_success(
+    mocked_redirect, mocked_post, mocked_parser, payment, provider
+):
+    transaction_id = "1234"
+    request = MagicMock()
+    request.GET = {"trans": transaction_id}
+    mocked_parser.return_value = {
+        "transactions": {
+            "transaction_details": {
+                "status": "ok",
+                "sender": {"holder": "John Doe", "country_code": "EN"},
+            }
         }
-        with self.assertRaises(RedirectNeeded):
-            self.provider.get_form(self.payment)
+    }
+    provider.process_data(payment, request)
+    assert payment.status == PaymentStatus.CONFIRMED
+    assert payment.captured_amount == payment.total
+    assert payment.transaction_id == transaction_id
 
-    @patch("xmltodict.parse")
-    @patch("requests.post")
-    @patch("payments.sofort.redirect")
-    def test_provider_redirects_on_success(
-        self, mocked_redirect, mocked_post, mocked_parser
-    ):
-        transaction_id = "1234"
-        request = MagicMock()
-        request.GET = {"trans": transaction_id}
-        mocked_parser.return_value = {
+
+@patch("xmltodict.parse")
+@patch("requests.post")
+@patch("payments.sofort.redirect")
+def test_provider_redirects_on_failure(
+    mocked_redirect, mocked_post, mocked_parser, payment, provider
+):
+    transaction_id = "1234"
+    request = MagicMock()
+    request.GET = {"trans": transaction_id}
+    mocked_parser.return_value = {}
+    provider.process_data(payment, request)
+    assert payment.status == PaymentStatus.REJECTED
+    assert payment.captured_amount == 0
+    assert payment.transaction_id == transaction_id
+
+
+@patch("xmltodict.parse")
+@patch("requests.post")
+def test_provider_refunds_payment(mocked_post, mocked_parser, payment, provider):
+    payment.extra_data = json.dumps(
+        {
             "transactions": {
                 "transaction_details": {
                     "status": "ok",
-                    "sender": {"holder": "John Doe", "country_code": "EN"},
+                    "sender": {
+                        "holder": "John Doe",
+                        "country_code": "EN",
+                        "bic": "1234",
+                        "iban": "abcd",
+                    },
                 }
             }
         }
-        self.provider.process_data(self.payment, request)
-        self.assertEqual(self.payment.status, PaymentStatus.CONFIRMED)
-        self.assertEqual(self.payment.captured_amount, self.payment.total)
-        self.assertEqual(self.payment.transaction_id, transaction_id)
-
-    @patch("xmltodict.parse")
-    @patch("requests.post")
-    @patch("payments.sofort.redirect")
-    def test_provider_redirects_on_failure(
-        self, mocked_redirect, mocked_post, mocked_parser
-    ):
-        transaction_id = "1234"
-        request = MagicMock()
-        request.GET = {"trans": transaction_id}
-        mocked_parser.return_value = {}
-        self.provider.process_data(self.payment, request)
-        self.assertEqual(self.payment.status, PaymentStatus.REJECTED)
-        self.assertEqual(self.payment.captured_amount, 0)
-        self.assertEqual(self.payment.transaction_id, transaction_id)
-
-    @patch("xmltodict.parse")
-    @patch("requests.post")
-    def test_provider_refunds_payment(self, mocked_post, mocked_parser):
-        self.payment.extra_data = json.dumps(
-            {
-                "transactions": {
-                    "transaction_details": {
-                        "status": "ok",
-                        "sender": {
-                            "holder": "John Doe",
-                            "country_code": "EN",
-                            "bic": "1234",
-                            "iban": "abcd",
-                        },
-                    }
-                }
-            }
-        )
-        mocked_parser.return_value = {}
-        self.provider.refund(self.payment)
-        self.assertEqual(self.payment.status, PaymentStatus.REFUNDED)
+    )
+    mocked_parser.return_value = {}
+    provider.refund(payment)
+    assert payment.status == PaymentStatus.REFUNDED
