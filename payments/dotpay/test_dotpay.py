aa85b4ddc518c599a6f44086cb229561e2a74fca
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2025-09-02 19:02:57 +0200

    Rewrite Dotpay tests in pytest-style

--- payments/dotpay/test_dotpay.py
+++ payments/dotpay/test_dotpay.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
 import hashlib
-from unittest import TestCase
 from unittest.mock import MagicMock
 from unittest.mock import Mock
 
+import pytest
 from django.http import HttpResponse
 from django.http import HttpResponseForbidden
 
@@ -37,7 +37,7 @@ PROCESS_POST = {
 
 def get_post_with_sha256(post):
     post = post.copy()
-    post["pin"] = "123"
+    post["pin"] = PIN
     keys = [
         "pin",
         "id",
@@ -59,8 +59,7 @@ def get_post_with_sha256(post):
     key = "".join([post[key] for key in keys])
     sha256 = hashlib.sha256()
     sha256.update(key.encode("utf-8"))
-    key_hash = sha256.hexdigest()
-    post["signature"] = key_hash
+    post["signature"] = sha256.hexdigest()
     return post
 
 
@@ -84,71 +83,69 @@ class Payment(Mock):
         self.status = status
 
 
-class TestDotpayProvider(TestCase):
-    def setUp(self):
-        self.payment = Payment()
+@pytest.fixture
+def payment():
+    return Payment()
 
-    def test_get_hidden_fields(self):
-        """DotpayProvider.get_hidden_fields() returns a dictionary"""
-        provider = DotpayProvider(seller_id="123", pin=PIN)
-        self.assertEqual(type(provider.get_hidden_fields(self.payment)), dict)
 
-    def test_process_data_payment_accepted(self):
-        """DotpayProvider.process_data() returns a correct HTTP response"""
-        request = MagicMock()
-        request.POST = get_post_with_sha256(PROCESS_POST)
-        params = {
-            "seller_id": 123,
-            "pin": PIN,
-            "endpoint": "test.endpoint.com",
-            "channel": 1,
-            "lang": "en",
-            "lock": True,
-        }
-        provider = DotpayProvider(**params)
-        response = provider.process_data(self.payment, request)
-        self.assertEqual(type(response), HttpResponse)
-        self.assertEqual(self.payment.status, PaymentStatus.CONFIRMED)
+def test_get_hidden_fields(payment):
+    provider = DotpayProvider(seller_id="123", pin=PIN)
+    assert isinstance(provider.get_hidden_fields(payment), dict)
 
-    def test_process_data_payment_rejected(self):
-        """DotpayProvider.process_data() returns a correct HTTP response"""
-        data = dict(PROCESS_POST)
-        data.update({"operation_status": REJECTED})
-        request = MagicMock()
-        request.POST = get_post_with_sha256(data)
-        params = {
-            "seller_id": 123,
-            "pin": PIN,
-            "endpoint": "test.endpoint.com",
-            "channel": 1,
-            "lang": "en",
-            "lock": True,
-        }
-        provider = DotpayProvider(**params)
-        response = provider.process_data(self.payment, request)
-        self.assertEqual(type(response), HttpResponse)
-        self.assertEqual(self.payment.status, PaymentStatus.REJECTED)
 
-    def test_incorrect_process_data(self):
-        """DotpayProvider.process_data() checks POST signature"""
-        request = MagicMock()
-        request.POST = PROCESS_POST
-        provider = DotpayProvider(seller_id="123", pin=PIN)
-        response = provider.process_data(self.payment, request)
-        self.assertEqual(type(response), HttpResponseForbidden)
+def test_process_data_payment_accepted(payment):
+    request = MagicMock()
+    request.POST = get_post_with_sha256(PROCESS_POST)
+    provider = DotpayProvider(
+        seller_id=123,
+        pin=PIN,
+        endpoint="test.endpoint.com",
+        channel=1,
+        lang="en",
+        lock=True,
+    )
+    response = provider.process_data(payment, request)
+    assert isinstance(response, HttpResponse)
+    assert payment.status == PaymentStatus.CONFIRMED
 
-    def test_uses_channel_groups_when_set(self):
-        channel_groups = "K,T"
-        params = {
-            "seller_id": 123,
-            "pin": PIN,
-            "endpoint": "test.endpoint.com",
-            "channel": 1,
-            "channel_groups": channel_groups,
-            "lang": "en",
-            "lock": True,
-        }
-        provider = DotpayProvider(**params)
-        hidden_fields = provider.get_hidden_fields(self.payment)
-        self.assertEqual(hidden_fields["channel_groups"], channel_groups)
-        self.assertIsNone(hidden_fields.get("channel"))
+
+def test_process_data_payment_rejected(payment):
+    data = dict(PROCESS_POST)
+    data.update({"operation_status": REJECTED})
+    request = MagicMock()
+    request.POST = get_post_with_sha256(data)
+    provider = DotpayProvider(
+        seller_id=123,
+        pin=PIN,
+        endpoint="test.endpoint.com",
+        channel=1,
+        lang="en",
+        lock=True,
+    )
+    response = provider.process_data(payment, request)
+    assert isinstance(response, HttpResponse)
+    assert payment.status == PaymentStatus.REJECTED
+
+
+def test_incorrect_process_data(payment):
+    request = MagicMock()
+    request.POST = PROCESS_POST  # no signature
+    provider = DotpayProvider(seller_id="123", pin=PIN)
+    response = provider.process_data(payment, request)
+    assert isinstance(response, HttpResponseForbidden)
+
+
+def test_uses_channel_groups_when_set(payment):
+    channel_groups = "K,T"
+    provider = DotpayProvider(
+        seller_id=123,
+        pin=PIN,
+        endpoint="test.endpoint.com",
+        channel=1,
+        channel_groups=channel_groups,
+        lang="en",
+        lock=True,
+    )
+    hidden_fields = provider.get_hidden_fields(payment)
+    assert hidden_fields["channel_groups"] == channel_groups
+    assert hidden_fields.get("channel") is None
